e5f8bf37694c8901b56cb9549ec729345ed5e289
Alecsander <alecsandertreds@uni9.edu.br>
2024-06-28 20:16:26 -0300

    Adicionando o Inicio do Projeto Alura me Java Script Numero Secreto

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert("Boas Vindas ao jogo do número secreto"); alert("Olá Vamos começar");
+let numeroSecreto = 29;
+let numeroChute = prompt("Qual valor do numero secreto de 0 a 100");
+
+if (numeroSecreto == numeroChute){
+    console.log("Você Acertou");
+}
+else{
+    console.log("Errou");
+}
