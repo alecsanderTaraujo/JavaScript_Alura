fd7a8636763d4189054090ce73d1d6a4bf5cf897
Alecsander <alecsandertreds@uni9.edu.br>
2024-07-03 12:28:15 -0300

    Atualização do programa com Math.random para gerar numeros aleatorios de 0 a 100

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,28 @@
-alert("Boas Vindas ao jogo do número secreto"); alert("Olá Vamos começar");
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let numeroChute = prompt("Qual valor do numero secreto de 0 a 100");
+alert("Boas Vindas ao jogo do número secreto");
+let numeroSecreto = parseInt(Math.random () * 100 + 1);
+console.log(numeroSecreto);
+let numeroChute;
+let tentativas = 1;
 
 
-if (numeroSecreto == numeroChute){
-    alert(`Você Acertou o número secreto ${numeroSecreto}`);
+while(numeroChute != numeroSecreto){
+    numeroChute = prompt("Qual valor do numero secreto de 0 a 100");
+    if (numeroSecreto == numeroChute){
+        break;
+    }
+    
+    else{
+        if(numeroSecreto > numeroChute){
+        alert(`Numero Secreto é maior do que o chute ${numeroChute}`);
+        } 
+        else{
+        alert(`Numero Secreto é menor do que o chute ${numeroChute}`);
+        }
+        tentativas++;
+    }
 }
-else{
-    alert(`Você Errou, Seu chute foi" ${numeroChute} e o Número Secreto era ${numeroSecreto}`);
-}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você Acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+
+
