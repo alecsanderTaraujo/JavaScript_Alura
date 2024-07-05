f21e85e36688bb1965ba11b8aa9b88e33bd314f1
Alecsander <alecsandertreds@uni9.edu.br>
2024-07-05 19:20:18 -0300

    Added Boas_praticas_funcao2_app.js and Boas_praticas_listas_app.js

---
+++ Desafios_logica/Boas_praticas_listas_app.js
@@ -0,0 +1,23 @@
+//1-Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//2-Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'Python', 'C++', 'Kotlin'];
+
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+//3-Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+listaDeNomes = ['Alec', 'Fernando', 'Talita'];
+console.log(listaDeNomes[0]);
+
+//4-Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+listaDeNomes = ['Alec', 'Fernando', 'Talita'];
+console.log(listaDeNomes[1]);
+
+
+//5Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+listaDeNomes = ['Alec', 'Fernando', 'Talita'];
+console.log(listaDeNomes[2]);
